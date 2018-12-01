9e103b0909ab957931d9ad7721ab0523c90c96c5
jarrelljohn@msn.com <jarrelljohn@msn.com>
2018-12-01 14:02:48 -0600

    added img updatedother files

--- day6/index.php
+++ day6/index.php
@@ -4,13 +4,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
   <title>Layout with Flexbox and CSS GRID</title>
 </head>
 <body>
   <div class="container">
-    <header class="mast">
-      <h1>We Are Glad To See You.</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, adipisci.</p>
+    <header id="mast">
+      <div id="header-content">
+        <h1>We Are Glad To See You.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, adipisci.</p>
+      </div>
+      
     </header>
   </div>
 </body>
