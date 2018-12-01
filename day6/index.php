dadb92d42a820d99d972df953f3aa35aa9f200f6
jarrelljohn@msn.com <jarrelljohn@msn.com>
2018-11-30 19:30:21 -0600

    changed index page

---
+++ day6/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Layout with Flexbox and CSS GRID</title>
+</head>
+<body>
+  <div class="container">
+    <header class="mast">
+      <h1>We Are Glad To See You.</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, adipisci.</p>
+    </header>
+  </div>
+</body>
+</html>
