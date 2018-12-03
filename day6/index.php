b214116f5c1e1a98d433a5d103291d69bded6c92
jarrelljohn@msn.com <jarrelljohn@msn.com>
2018-12-02 21:22:45 -0600

    Updated day 6

--- day6/index.php
+++ day6/index.php
@@ -11,13 +11,35 @@
   <div class="container">
     <header id="mast">
       <div id="header-content">
-        <h1>We Are Glad To See You.</h1>
+        <h1>Jitters Coffee Shoppe</h1>
+        <h2> we love our coffee and you do to.</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, adipisci.</p>
+        <a href="#about" class="button">Find out more</a>
       </div>
     </header>
     <section class="part-a">
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, dolor labore vitae magni odio nisi exercitationem amet voluptatem accusantium nostrum pariatur voluptates eligendi ipsum quo ab iusto, cupiditate maiores ullam ratione fugit aperiam cumque facilis alias! Pariatur dolore aliquam commodi numquam itaque! Nam iste iusto nesciunt numquam sed, culpa in blanditiis laboriosam laudantium delectus. Placeat omnis laborum animi in nam nihil, quo aliquid quis doloremque corrupti esse voluptas illo harum eos nostrum ut sint consequatur, laudantium aliquam! Corrupti, corporis. Facere possimus vitae libero neque quas inventore cum ea placeat. Maiores nobis adipisci quasi iste facilis perspiciatis accusantium autem obcaecati rerum.</p>
     </section>
+    <section class="part-b">
+      <a name="about"></a>
+      <div class="section2-content">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, dolor labore vitae magni odio nisi exercitationem amet voluptatem accusantium nostrum pariatur voluptates eligendi ipsum quo ab iusto, cupiditate maiores ullam ratione fugit aperiam cumque facilis alias! Pariatur dolore aliquam commodi numquam itaque! Nam iste iusto nesciunt numquam sed, culpa in blanditiis laboriosam laudantium delectus. Placeat omnis laborum animi in nam nihil, quo aliquid quis doloremque corrupti esse voluptas illo harum eos nostrum ut sint consequatur, laudantium aliquam! Corrupti, corporis. Facere possimus vitae libero neque quas inventore cum ea placeat. Maiores nobis adipisci quasi iste facilis perspiciatis accusantium autem obcaecati rerum.</p>
+        <div class="section-three">
+          <div class="one">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam corrupti ipsam excepturi atque eligendi possimus tempora magni. Facere, quibusdam aliquam.</p>
+          </div>
+          <div class="two">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam corrupti ipsam excepturi atque eligendi possimus tempora magni. Facere, quibusdam aliquam.</p>
+          </div>
+          <div class="three">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam corrupti ipsam excepturi atque eligendi possimus tempora magni. Facere, quibusdam aliquam.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <p>&copy; <?php echo date('Y'); ?> Jitters Coffee Shoppe. Website designed by jjarrellwebdev.com.</p>
+    </footer>
   </div>
 </body>
 </html>
